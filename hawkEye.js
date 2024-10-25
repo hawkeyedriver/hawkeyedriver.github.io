12d6a0eea22a64112488b198a84429b2071cbad1
Raymond Schenk <rayschenk@gmail.com>
2024-10-25 15:36:57 -0400

    Commit root files only

---
+++ hawkEye.js
@@ -0,0 +1,5 @@
+<script>
+    // Check if the user is not on the homepage
+    if (window.location.pathname !== '/' && window.location.pathname !== '/homePage.html') {
+        document.getElementById('backButton').style.display = 'inline-block'}
+</script>
